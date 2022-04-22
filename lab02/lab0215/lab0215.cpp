e2452962f81cb4df856409f96a45435cfdb14513
Dima Andronik <andronikdima3@gmail.com>
2022-04-22 13:22:06 +0300

    lab0215

--- lab02/lab0215/lab0215.cpp
+++ lab02/lab0215/lab0215.cpp
@@ -1,30 +1,51 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
-
 int main() {
-    int F, a, b, c, x;
 
-    cout << "Enter F" << endl;
-    cin >> F;
-    cout << "Enter a" << endl;
-    cin >> a;
-    cout << "Enter b" << endl;
-    cin >> b;
-    cout << "Enter c" << endl;
-    cin >> c;
-    cout << "Enter x" << endl;
-    cin >> x;
+    int months;
+    cout << "Enter a number from 1 to 12" << endl;
+    cin >> months;
+    switch (months) {
 
-    if ((x = 0) && (b != 0)) {
-        F = ((a*(x + c)) * 2);
+    case (1):
+        cout << "January";
+        break;
+    case (2):
+        cout << "February";
+        break;
+    case (3):
+        cout << "March";
+        break;
+    case (4):
+        cout << "April";
+        break;
+    case (5):
+        cout << "May";
+        break;
+    case (6):
+        cout << "June";
+        break;
+    case (7):
+        cout << "July";
+        break;
+    case (8):
+        cout << "August";
+        break;
+    case (9):
+        cout << "September";
+        break;
+    case (10):
+        cout << "October";
+        break;
+    case (11):
+        cout << "November";
+        break;
+    case (12):
+        cout << "December";
+        break;
+
     }
-    else if ((x = 0) && (b = 0)){
-        F = ((x - a) / (-c));
-    }
-    else {
-        F = (a + (x / c));
-    }
-    cout << F;
 
 }
+
